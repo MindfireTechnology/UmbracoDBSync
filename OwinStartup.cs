4d9d992d93e21ee060f9f489fe3f0c8ee6ac6345
Nate Zaugg <nzaugg@gmail.com>
2016-02-08 18:26:44 -0700

    Fixing build to be indepentend of the project I pulled it from

---
+++ OwinStartup.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+[assembly: OwinStartup(typeof(UmbracoDbSync.OwinStartup), "DbSyncStartup")]
+
+namespace UmbracoDbSync
+{
+	public sealed class OwinStartup
+	{
+		public void DbSyncStartup(IAppBuilder app)
+		{
+			string configFile = Directory.EnumerateFiles(GetHostPath(), "TableMappings.config", SearchOption.AllDirectories).FirstOrDefault();
+
+		}
+
+		private string GetHostPath()
+		{
+			// Getting the path in a somewhat platform agnostic way.
+			return Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+		}
+	}
+}
