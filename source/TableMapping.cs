e433dcd31d7d7ea41d4f7468d21f0407692dfd38
nzaugg <nzaugg@gmail.com>
2016-02-15 14:53:37 -0700

    Rearranging structure

---
+++ source/TableMapping.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UmbracoDbSync
+{
+	public class TableMapping
+	{
+		/// <summary>Table Name</summary>
+		public string Name { get; set; }
+		public string DocumentType { get; set; }
+		public string Assembly { get; set; }
+		public string Namespace { get; set; }
+		public string EntityTypeFullName { get; set; }
+		public string EntityTypeName { get { return EntityTypeFullName.Substring(EntityTypeFullName.LastIndexOf(".") + 1); } }
+		public string EntityPropertyName { get; set; }
+
+		public bool AutoMapFields { get; set; }
+		public bool AllowDelete { get; set; }
+
+		public IList<FieldMapping> FieldMappings { get; set; }
+
+		public TableMapping()
+		{
+			FieldMappings = new List<FieldMapping>();
+			AutoMapFields = true;
+		}
+	}
+}
